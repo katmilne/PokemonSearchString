0cf73cf37695962dda0e196d34efe6ba00b789b8
Kat M <kat.milne@hotmail.co.uk>
2024-11-14 17:59:02 +0000

    addec opy to clipbaord button

--- app/components/SearchBuilder.tsx
+++ app/components/SearchBuilder.tsx
@@ -6,11 +6,33 @@ interface SearchBuilderProps {
 }
 
 const SearchBuilder: React.FC<SearchBuilderProps> = ({ searchString }) => {
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(searchString)
+      .then(() => alert("Search string copied to clipboard!"))
+      .catch(err => alert("Failed to copy text: " + err));
+  };
+
   return (
-    <div className="p-4 bg-gray-100 rounded-lg shadow-md w-[80%] my-6 mx-auto">
-      <p className={`text-xl ${searchString ? 'text-gray-800 text-center' : 'text-gray-500 italic text-center'}`}>
-        {searchString || "Click buttons to build your search!"}
-      </p>
+    <div className="flex justify-center p-4 bg-gray-100 rounded-lg shadow-md w-[80%] my-6 mx-auto">
+
+      <div className='w-full'>
+        <p className={`text-xl ${searchString ? 'text-gray-800 text-center' : 'text-gray-500 italic text-center'}`}>
+          {searchString || "Click buttons to build your search!"}
+        </p>
+      </div>
+
+      <div className='flex justify-end pr-2'>
+        {/* Copy Button */}
+        <button
+          onClick={copyToClipboard}
+          className="bg-slate-300 p-1 rounded-md hover:bg-slate-400 "
+        >
+          Copy
+        </button>
+      </div>
+      
+
     </div>
   );
 };
